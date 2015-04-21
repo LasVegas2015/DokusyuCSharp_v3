1c7c92c4197dcfe185800e928ae7f6610358dcdc
LasVegas2015 <drivemylife@gmail.com>
2015-04-21 22:38:20 +0900

    2015/04/21

---
+++ Chapter1/Check_1_11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Check_1_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight = 67.5;
+            double weightOnMoon = GetWeightOnMoon(weight);
+
+            Console.WriteLine("Weight on the Moon: " + weightOnMoon);
+        }
+
+        private static double GetWeightOnMoon(double weight)
+        {
+            return weight * 0.83;
+        }
+    }
+}
---
+++ Chapter1/Check_1_12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Check_1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch;
+            double meter;
+
+            int counter = 0;
+            for (inch = 1; inch <= 144; inch++)
+            {
+                meter = inch * 39.37;
+                Console.WriteLine(inch + " inches are " + meter + " meters.");
+
+                counter++;
+                if (counter == 12)
+                {
+                    Console.WriteLine();
+                    counter = 0;
+                }
+            }
+        }
+    }
+}
