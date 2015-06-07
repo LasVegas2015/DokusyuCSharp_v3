ddc96e264e6f1be20885b8e122954673fe73753d
LasVegas2015 <drivemylife@gmail.com>
2015-06-07 22:34:09 +0900

    2015/06/07 Check_3_10

---
+++ Chapter3/Check_3_10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Check_3_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 1000000; i *= 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
