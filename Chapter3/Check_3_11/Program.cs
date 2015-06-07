73e8fea0fb9e0e3946492b15f31925ac036be253
LasVegas2015 <drivemylife@gmail.com>
2015-06-07 23:20:05 +0900

    2015/06/07 Check_3_11

---
+++ Chapter3/Check_3_11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Check_3_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            int  counter = 0;
+
+            for ( ; ; )
+            {
+                do
+                {
+                    ch = (char)Console.Read();
+                } while (ch == '\n' || ch == '\r');
+
+                if (ch == '.')
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(counter);
+                    break;
+                }
+
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    ch -= (char)32;
+                    counter++;
+                    Console.Write(ch);
+                }
+                else if (ch >= 'A' && ch <= 'Z')
+                {
+                    ch += (char)32;
+                    counter++;
+                    Console.Write(ch);
+                }
+                else
+                {
+                    Console.Write(ch);
+                }
+            }
+        }
+    }
+}
